c73b85b8d3841f62fa2a0a83544cd2c9276734d4
Carsten Eie Frigaard <cef@ase.au.dk>
2021-06-28 12:40:11 +0200

    update

--- Etc/mk_course_plan.py
+++ Etc/mk_course_plan.py
@@ -8,100 +8,174 @@ from argparse import ArgumentParser
 from html import escape
  
 if __name__ == '__main__':
+	
+	def isStr(t):
+		assert isinstance(t, str)
+		#assert len(t)>0
+		return t
 
-	def LoadText(filename, timeout=4000):
-		assert isinstance(filename, str)
-		with open(filename, 'r', timeout) as f:
-			c = f.read().split("\n")
-			return c
+	def isBool(t):
+		assert isinstance(t, bool)
+		return t
 
-	def ParseWidths(headers):
-		assert isinstance(headers, list)
-		w = []
-		h = []
-		for i in headers:
-			curr = 65
-			if i[-1]=="}":
-				n = i.find("{")
-				if n>=0:
-					curr = int(i[n+1:-1])
-					#print(curr)
-					h.append(i[:n])
-			else:
-				h.append(i)
-				
-			assert isinstance(curr, int) and curr>=0
-			w.append(curr)
+	def isInt(t, minval=0):
+		assert isinstance(t, int)
+		assert t>=minval
+		return t
 		
-		assert len(w)==len(headers) and len(w)==len(h)
-		return w, h		
-			
-	def ParseLine(h, expectedlen):
+	def isList(t):
+		assert isinstance(t, list)
+		return t
+	
+	def isTuple(t, expectedlen=2):
+		assert isinstance(t, tuple)
+		assert len(t)==isInt(expectedlen, 1)
+		return t
+	
+	def LoadText(filename, timeout=4000, split=True):
+		with open(isStr(filename), 'r', timeout) as f:
+			c = f.read()
+			if split:
+				c= c.split("\n")
+			return c
+	
+	def ParseStructure(planlist):
+
+		def Check(expr, msg):
+			isStr(msg)
+			if not expr:
+				Err("EXPRESSION NOT FULLFILLED: " + msg)
+	
 		def Trim(s):
-			assert isinstance(s, str)
-			s = s.replace("\t"," ")
+			s = isStr(s).replace("\t"," ")
 			s = s.strip()
 			return s
 
-		def Encode(s):
-			assert isinstance(s, str)
-			return escape(s)
+		def ParseWidths(headers):
+			w = []
+			h = []
+			for i in isList(headers):
+				curr = 65
+				if i[-1]=="}":
+					n = i.find("{")
+					if n>=0:
+						curr = int(i[n+1:-1])
+						#print(curr)
+						h.append(i[:n])
+				else:
+					h.append(i)
+					
+				w.append(isInt(curr, 0))
+			
+			assert len(w)==len(headers) and len(w)==len(h)
+			return w, h		
+				
+		def ParseLine(h, expectedlen):
+			s = isStr(h).split("|")
+			N = len(s)
+			
+			if N != expectedlen:
+				if expectedlen < 0 and N < 2:
+					ERR(f"expected more than one column in header '{h}', but got {N} column(s)")
+				elif expectedlen >= 0:
+					ERR(f"expected exaclty {expectedlen} column(s) in line '{h}', but got {N} column(s)")
+			
+			r = []
+			o = ""
+			for i in s:
+				t = Trim(i)
+				o += ("     " if expectedlen < 0 else "  ") + t + ("\n" if expectedlen < 0 else " | ") 
+				r.append(t)
+			print(o)
+			
+			return r
+		
+		def Check(expr, msg):
+			isStr(msg)
+			if not expr:
+				ERR("CONDITION NOT FULLFILLED: " + msg)
 
-		assert isinstance(h, str)
+		def CheckLine(i, expected):
+			Check(i<len(planlist),        "i={i} < len of planlist={len(planlist}") 
+			Check(planlist[i]==expected, f"planlist missing tag '{expected}' at line {i}, got='{planlist[i]}'")
+						
+		N = len(isList(planlist))
+		if N < 7:
+			ERR("planlist too short, expected at least three lines with COUSEPLAN/HEAD/CONTENT/REFS/END")
 		
-		s = h.split("|")
-		N = len(s)
-		
-		if N != expectedlen:
-			if expectedlen < 0 and N < 2:
-				ERR(f"expected more than one column in header '{h}', but got {N} column(s)")
-			elif expectedlen >= 0:
-				ERR(f"expected exaclty {expectedlen} column(s) in line '{h}', but got {N} column(s)")
-		
-		r = []
-		o = ""
-		for i in s:
-			t = Trim(i)
-			o += ("     " if expectedlen < 0 else "  ") + t + ("\n" if expectedlen < 0 else " | ") 
-			r.append(Encode(t))
-		print(o)
-		
-		return r
-		
-	def GenerateHtml(headers, widths, parsed, fullhtmldoc):
-		assert isinstance(headers, list)
-		assert isinstance(widths, list)
-		assert isinstance(parsed, list)
-		assert isinstance(fullhtmldoc, bool)		
-		
+		CheckLine(0, "COUSEPLAN")
+		CheckLine(1, "HEAD")
+		CheckLine(3, "CONTENT")
+
+		headers = ParseLine(planlist[2], -1)
+		widths, headers = ParseWidths(headers)		
 		M = len(headers)
-		html = '<!DOCTYPE html>\n<html>\n<body>\n' if fullhtmldoc else ""
+		
+		s = {}
+		s['headers'] = headers
+		s['widths']  = widths
+		
+		curr = []
+		
+		for j in range(4, N):
+			i = isStr(Trim(planlist[j]))
+			
+			if i=="REFS":
+				s['content']=curr
+				curr = []
+			if i=="END":
+				e = True
+				s['refs']=curr
+				curr = None
+				break
+			curr.append(i)
+					
+		Check(curr is None, 	"plainlist missing END tag")
+		Check(s.get('content'), "planlist missing content structure")
+		Check(s.get('refs'),    "planlist missing ref structure")
+
+		parsed = []
+		for i in s['content']:
+			l = ParseLine(i, M)
+			if args.v > 0:
+				print(l, stderr)
+			parsed.append(l)
+		s['parsed'] = parsed
+	
+		return s
+
+	def GenerateHtml(headers, widths, parsed, fullhtmldoc):
+		def HtmlEncode(s):
+			return escape(isStr(s))
+		
+		M = len(isList(headers))
+		html = '<!DOCTYPE html>\n<html>\n<body>\n' if isBool(fullhtmldoc) else ""
 		html += '<table cellspacing="0px" cellpadding="1px" border="1px" align="center">\n<tbody>\n<tr style="background-color: #000000;" align="center">\n'		
-		for i in zip(headers, widths):
-			assert isinstance(i, tuple) and len(i)==2
-			assert isinstance(i[0], str)
-			assert isinstance(i[1], int) and i[1]>=0
-			html += f'<td width="{i[1]}"><span style="background-color: #000000; color: #ffffff;"><strong>{i[0]}</strong></span></td>\n'
+		
+		for i in zip(isList(headers), isList(widths)):
+			isTuple(i)
+			html += f'<td width="{isInt(i[1])}"><span style="background-color: #000000; color: #ffffff;"><strong>{HtmlEncode(i[0])}</strong></span></td>\n'
 		html += '</tr>\n'
 
-		for j in parsed:
-			assert isinstance(j, list)
-			assert len(j)==M
+		for j in isList(parsed):
+			assert len(isList(j))==M
 			html += '<tr align="center">\n'
 			
 			assert len(j)==len(widths)
 			for k in zip(j, widths):
-				assert isinstance(k, tuple) and len(k)==2
-				assert isinstance(k[0], str)
-				assert isinstance(k[1], int) and k[1]>=0
-				html += f'<td width="{k[1]}">{k[0]}</td>\n'
+				isTuple(k)
+				html += f'<td width="{isInt(k[1])}">{HtmlEncode(k[0])}</td>\n'
 			html += '</tr>'
+		
 		html += '</table>\n'
-		html += '</body>\n</html>' if fullhtmldoc else ""
+		html += '</body>\n</html>' if isBool(fullhtmldoc) else ""
 		
 		return html
 		
 	def Usage(msg=""):
+		def test():
+			print("test")
+			
 		print("Usage: mk_course_plan.py [-v] [-t] [-p <planfile>] [-o <outputfile>]") 
 		print("  version 0.1")
 		print("Args:")
@@ -116,7 +190,7 @@ if __name__ == '__main__':
 	try:		
 		planfile = "plan.txt"
 		outputfile= "index.html"
-			
+		
 		parser = ArgumentParser()
 		parser.add_argument("-v", default=0,          action="count",      help= "increase output verbosity, default=0\n")
 		parser.add_argument("-t", default=False,      action="store_true", help=f"generate simple table (witouth <html> <body> etc tags), default=False\n")
@@ -126,34 +200,15 @@ if __name__ == '__main__':
 		
 		planfile = args.p
 		print(f"GENERATING html plan from file '{planfile}'..")
-		planlist = LoadText(planfile)
-		assert isinstance(planlist, list)
 		
-		N = len(planlist)
-		if N<3:
-			ERR(f"expected more that three lines in planfile '{planfile}', but got {N} lines")
-		if planlist[0]!="COUSEPLAN":
-			ERR(f"expected tag 'COUSEPLAN' in header of planfile '{planfile}', but got '{planlinst[0]}'")
-		
-		headers = ParseLine(planlist[1], -1)
-		widths, headers = ParseWidths(headers)
-		
-		M = len(headers)
+		structure = ParseStructure(LoadText(planfile))		
+	
 		if args.v > 0:
-			print(headers, stderr)
+			print(structure['headers'], stderr)
 		
-		parsed = []
-		for i in range(2, N):
-			l = ParseLine(planlist[i], M)
-			if args.v > 0:
-				print(l, stderr)
-			parsed.append(l)
-			
-		html = GenerateHtml(headers, widths, parsed, not args.t)
+		html = GenerateHtml(structure['headers'], structure['widths'], structure['parsed'], not args.t)
 		
-		outputfile = args.o
-		assert isinstance(outputfile, str)
-		with open(outputfile, 'w') as f:
+		with open(isStr(args.o), 'w') as f:
 			f.write(html)
 		
 		print("DONE")
