ce36fff02e1847d8dd33dd15e2206191a6288faa
Carsten Eie Frigaard <cef@ece.au.dk>
2021-09-01 13:22:03 +0200

    update

--- libitmal/kernelfuns.py
+++ libitmal/kernelfuns.py
@@ -1,63 +1,4 @@
 #!/usr/bin/env python3
-
-def UsesGPU():    
-	TF_gpu = False
-	K_gpu  = False    
-	
-	try:
-		# confirm TensorFlow sees the GPU
-		from tensorflow.python.client import device_lib
-		if 'GPU' in str(device_lib.list_local_devices()):
-			TF_gpu = True      
-	except:
-		print("WARNING: could not import from tensorflow"); 
-    
-	# NOTE: will not work under some keras/TF versions..
-	#try:
-	#    # confirm Keras sees the GPU
-	#    from keras import backend
-	#    if len(backend.tensorflow_backend._get_available_gpus()) > 0:
-	#        K_gpu = True
-	#except:
-	#    print("WARNING: could not import from keras");
-	
-	return TF_gpu, K_gpu
-
-def DisableGPU():
-	import os
-	os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-	os.environ["CUDA_VISIBLE_DEVICES"] = ""
-	TF_gpu, K_gpu = UsesGPU()
-	#if K_gpu:
-	#    raise ImportException("no keras import allowed before calling DisableGPU(), reset your kernel and call DisableGPU() in the beginning of your code.")
-
-def EnableGPU(gpu_mem_fraction=0.05, gpus=None, cpus=None):
-	#try:
-	#    import os
-	#    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-	#    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-	#except:
-	#    print("ERROR: something failed in EnableGPU(), environment part")
-	
-	try:
-		import tensorflow as tf
-		from keras.backend.tensorflow_backend import set_session
-		config = tf.ConfigProto()        
-		if gpus!=None or cpus!=None:
-			if gpus==None:
-				gpus=1 # default
-			if cpus==None:
-				cpus=4 # default
-			config = tf.ConfigProto( device_count = { 'GPU': gpus, 'CPU': cpus}  )
-			if gpu_mem_fraction<0:
-				raise ValueExeption("gpu_mem_fraction is below zero")
-			if gpu_mem_fraction>0.5:
-				raise ValueExeption("sorry Dave, can't do gpu_mem_fraction>50%")
-		config.gpu_options.per_process_gpu_memory_fraction = gpu_mem_fraction
-		config.gpu_options.allow_growth=True
-		set_session(tf.Session(config=config))
-	except:
-		print("ERROR: something failed in EnableGPU(), Tensorflow part")
      
 def RestartKernel() :
 	try:
@@ -66,9 +7,69 @@ def RestartKernel() :
 	except:
 		print("ERROR in RestartKernel()!")		
 
+def ResetSeeds(seed=42):
+	import numpy as np
+	import tensorflow as tf
+	import random as python_random
+	
+	from os import environ
+	environ['PYTHONHASHSEED']=str(seed)
+	
+	np.random.seed(seed) 
+	python_random.seed(seed)
+	tf.random.set_seed(seed)
+
+def DisableGPUs():
+	import os
+	os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+	#try:
+	#	# Disable all GPUS
+	#	tf.config.set_visible_devices([], 'GPU')
+	#	visible_devices = tf.config.get_visible_devices()
+	#	for device in visible_devices:
+	#		assert device.device_type != 'GPU'
+	#except:
+	#	# Invalid device or cannot modify virtual devices once initialized.
+	#	WARN("exception in DisableGPUs() ignored")
+
+def ReEnableGPUs():
+	# if DisableGPUs() was called, you can re-enable GPUS via this function
+	import os
+	os.environ["CUDA_VISIBLE_DEVICES"] = "" 
+
+def StartupSequence_GPU(verbose=False):
+	n = -1
+	if verbose:
+		print("StartupSequence_GPU()..")
+	try:
+		import tensorflow
+
+		physical_devices = tensorflow.config.list_physical_devices('GPU')
+		n = 0
+		for i in range(len(physical_devices)):
+			if verbose:
+				print(f"  setting physical_devices[{i}] set_memory_growth=True..")
+			tensorflow.config.experimental.set_memory_growth(physical_devices[i], True)
+			n += 1
+			
+	except Exception as e:
+		if (verbose):
+			print(f"ERROR: something failed in StartupSequence_EnableGPU(), re-raising exception='{e}'\n")
+		raise e
+	
+	assert n > -1, f"something went wrong in startup-sequence, n={n} should be > -1"	
+	return n
+
+########################################
+
 def TestAll():
 	print("(not tests yet)")
 	print("ALL OK")
 
 if __name__ == '__main__':
 	TestAll()
+
+#Versions()
+#ResetSeeds()
+#DisableGPUs()
+#StartupSequence_GPU(verbose=False)
