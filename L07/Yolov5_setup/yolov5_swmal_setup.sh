a64e88ac3dff66fed7974d86a2f49cd45786de4a
Carsten Eie Frigaard <cef@ece.au.dk>
2022-04-01 15:24:53 +0200

    yolov5_update

--- L07/Yolov5_setup/yolov5_swmal_setup.sh
+++ L07/Yolov5_setup/yolov5_swmal_setup.sh
@@ -1,22 +1,39 @@
-q#!/bin/bash
+#!/bin/bash
 
 set -ea
 
 # NOTE: could create a new env. but most things are ok 
 #       when using anaconda-2021.11 ..
 
-#conda create --name yolov5
-#conda env list
-#conda activate yolov5
+function CreateEnv()
+{
+	conda create --name yolov5
+	conda env list
+	conda activate yolov5
+}
 
-(test ! -d yolov5 && git clone https://github.com/ultralytics/yolov5) || echo "Git-repository already cloned.."
+function Setup()
+{
+	(test ! -d yolov5 && git clone https://github.com/ultralytics/yolov5) || echo "Git-repository already cloned.."
 
-#cd yolov5
-#pip install -r requirements.txt 
+	#cd yolov5
+	#pip install -r requirements.txt 
 
-# NOTE: torch version in original requirements incompatible with GPU hardware, 
-#       use the following instead ..
-pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/torch_stable.html
+	# NOTE: torch version in original requirements incompatible with GPU hardware, 
+	#       use the following instead ..
+	pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/torch_stable.html
 
-# NOTE: then install whatever is not in conda ..
-pip install -r swmal_requirements.txt 
+	# NOTE: then install whatever is not in conda ..
+	pip install -r yolov5_swmal_requirements.txt
+}
+
+function UninstallPips()
+{
+	pip uninstall opencv-python tensorboard tensorboard-data-server thop torch torchaudio torchvision
+}
+
+#CreateEnv
+Setup
+#UninstallPips
+
+echo "DONE"
